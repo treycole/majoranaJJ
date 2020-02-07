e979942d97ce7b84e8dcc91d8a4eb4dfded027f2
Trey Cole <treco713@gmail.com>
2020-02-06 22:42:55 -0500

    Updated test

---
+++ lattice.py
@@ -0,0 +1,74 @@
+#defining lattice, numbered 0->N
+def lattice(Nx, Ny):
+    lattice = np.zeros((Nx, Ny))
+    for i in range(Ny):
+        for j in range(Nx):
+            lattice[i, j] = j + i*Ny
+    return lattice
+
+#defining coordinate array
+#coordinate array is Nx2, first column is array of x values in units of [A], second column is y values in units of [A]
+#creates a square or rectangular coordinate array, it stores i*ax as x coordinate, n = nx + nx*ny means ...
+# the lattice reference number is the x-value of the loop plus the y-value times the x-value
+def square(Nx, Ny, ax, ay):
+    N = Nx*Ny
+    coor = np.zeros((N,2))
+    for i in range(Nx):
+        for j in range(Ny):
+            n = i + Nx * j
+            x = (i) * ax
+            y = (j) * ay
+            coor[n,0] = x
+            coor[n,1] = y
+    return coor
+
+#defining nearest neighbor array
+#NN_arr is Nx4, the columns store the index of the nearest neighbors.
+#Left: NN[n,0] = (n-Nx), Above: NN[n,1] = n+1, Right: NN[n, 2] = n+1, Down NN[n, 3] = n+Nx
+#if there is no lattice site in nearest neighbor spot, value is -1
+def NN_Arr(coor, ax, ay):
+    N = coor.shape[0]
+    tol = 1e-8
+    NN = -1*np.ones((N,4), dtype = 'int')
+    for n in range(N):
+        for m in range(N):
+            xn = coor[n, 0]
+            xm = coor[m, 0]
+            yn = coor[n,1]
+            ym = coor[m,1]
+
+            if abs((xn - xm) - ax)< tol and abs(yn - ym) < tol:
+                NN[n, 0] = m
+            if abs((xn - xm) + ax) < tol and abs(yn - ym) < tol:
+                NN[n, 2] = m
+            if abs((yn - ym) + ay) < tol and abs(xn - xm) < tol:
+                NN[n, 1] = m
+            if abs((yn - ym) - ay) < tol and abs(xn - xm) < tol:
+                NN[n, 3]= m
+    return NN
+
+#Disk with a hole
+def donut(R, r, ax, ay):
+    CAx = []
+    CAy = []
+
+    xmin = -R #Radius of disk
+    ymin = -R
+
+    Nx = int(2*R/ax + 2)
+    Ny = int(2*R/ay + 2)
+
+    for j in range(Ny):
+        for i in range(Nx):
+            x = xmin + i*ax
+            y = ymin + j*ay
+            #decide if x,y is inside shape
+            r_ij = np.sqrt(x**2+y**2)
+            if r_ij < R and r_ij > r:
+                CAx.append(x)
+                CAy.append(y)
+
+    coor_arr = np.zeros((len(CAx), 2))
+    coor_arr[:, 0] = np.array(CAx)
+    coor_arr[:, 1] = np.array(CAy)
+    return coor_arr
---
+++ operators.py
@@ -0,0 +1,79 @@
+import numpy as np
+from numpy import linalg as LA
+import constants as const
+import lattice as lat
+
+# Descritizing $k_x$ and $k_y$
+def k_x(coor, ax, ay):
+    n = coor.shape[0]
+    k_x = np.zeros((N,N), dtype = "complex")
+    NN = lat.NN_Arr(coor, ax, ay)
+    for i in range(N):
+        for j in range(N):
+            if NN[j,0] == i:
+                k_x[j,i] = -1j/(2*ax)
+            if NN[j, 2] == i:
+                k_x[j,i] = 1j/(2*ax)
+    return k_x
+def k_y(coor, ax, ay):
+    N = coor.shape[0]
+    k_y = np.zeros((N,N), dtype = "complex")
+    NN = lat.NN_Arr(coor, ax, ay)
+    for i in range(N):
+        for j in range(N):
+            if NN[j,1] == i:
+                k_x[j,i] = 1j/(2*ax)
+            if NN[j, 3] == i:
+                k_x[j,i] = -1j/(2*ax)
+    return k_x
+
+#Descritizing kx^2 and ky^2
+def k_x2(coor, ax, ay):
+    N = coor.shape[0]
+    k_x2 = np.zeros((N,N), dtype='complex')
+    NN = lat.NN_Arr(coor, ax, ay)
+    for i in range(N):
+        for j in range(N):
+            if NN[j,0] == i:
+                k_x2[j,i] = -1/ax**2
+            if NN[j, 2] == i:
+                k_x2[j,i] = -1/ax**2
+            if i == j:
+                k_x2[j,i] = 2/ax**2
+    return k_x2
+
+def k_y2(coor, ax, ay):
+    N = coor.shape[0]
+    k_y2 = np.zeros((N,N), dtype='complex')
+    NN = lat.NN_Arr(coor, ax, ay)
+    for i in range(N):
+        for j in range(N):
+            if NN[j,1] == i:
+                k_y2[j,i] = -1/ax**2
+            if NN[j, 3] == i:
+                k_y2[j,i] = -1/ax**2
+            if i == j:
+                k_y2[j,i] = 2/ax**2
+    return k_y2
+
+#Defining Hamiltonian for simple free particle in the lattice
+def H0(coor, ax, ay):
+    N = coor.shape[0]
+    H = np.zeros((N,N), dtype = 'complex')
+    H = const.hbar**2/(2*const.m0)*(k_x2(coor, ax, ay) + k_y2(coor, ax, ay))
+    return H
+
+#Getting energies
+def E0(coor, ax, ay):
+    N = coor.shape[0]
+    H = H0(coor, ax, ay)
+    print (H.shape)
+    eigvals, eigvecs = LA.eigh(H)
+    return np.sort(eigvals)
+
+#Getting States
+def eigstate(coor, ax, ay):
+    N = coor.shape[0]
+    H = H0(coor, ax, ay)
+    eigvals, eigvecs = LA.eigh(H)
+    return eigvecs
