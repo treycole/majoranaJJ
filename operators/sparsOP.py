0ae92446c889833fc563519c0af77f1deb053c3b
Trey Cole <treco713@gmail.com>
2020-03-31 18:50:31 -0400

    sparse delta

--- operators/sparsOP.py
+++ operators/sparsOP.py
@@ -1,6 +1,6 @@
 import scipy.sparse as sparse
 import numpy as np
-import majoranaJJ.const as const
+import majoranaJJ.constants as const
 
 def kx(coor, ax, ay, NN, NNb = None, qx = 0):
     row = []; col = []; data = []
@@ -22,7 +22,7 @@ def kx(coor, ax, ay, NN, NNb = None, qx = 0):
         if NNb != None and NNb[i,2] != -1:
             row.append(NNb[i,2]); col.append(i); data.append(tx*np.exp(1j*qx*Lx))
 
-    ksq = sparse.csr_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
+    ksq = sparse.csc_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
     return ksq
 
 def kx2(coor, ax, ay, NN, NNb = None, qx = 0):
@@ -47,7 +47,7 @@ def kx2(coor, ax, ay, NN, NNb = None, qx = 0):
             row.append(NNb[i,2]); col.append(i); data.append(-tx*np.exp(1j*qx*Lx))
 
 
-    ksq = sparse.csr_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
+    ksq = sparse.csc_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
     return ksq
 
 def ky(coor, ax, ay, NN, NNb = None, qy = 0):
@@ -70,7 +70,7 @@ def ky(coor, ax, ay, NN, NNb = None, qy = 0):
         if NNb != None and NNb[i,3] != -1:
             row.append(NNb[i,3]); col.append(i); data.append(ty*np.exp(1j*qy*Ly))
 
-    ksq = sparse.csr_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
+    ksq = sparse.csc_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
     return ksq
 
 def ky2(coor, ax, ay, NN, NNb = None, qy = 0):
@@ -94,30 +94,39 @@ def ky2(coor, ax, ay, NN, NNb = None, qy = 0):
         if NNb != None and NNb[i,3] != -1:
             row.append(NNb[i,3]); col.append(i); data.append(-ty*np.exp(1j*qy*Ly))
 
-    ksq = sparse.csr_matrix((data, (row,col)), shape= (N,N), dtype = 'complex')
+    ksq = sparse.csc_matrix((data, (row,col)), shape= (N,N), dtype = 'complex')
     return ksq
 
 def Delta(coor, delta, Wsc, Wj, phi = 0, Sx = 0, Sy = 0, cutx = 0, cuty = 0):
     N = coor.shape[0]
-    row = []; col = []; data = []
+    row = []; col = []; data01 = []
+    row = []; col = []; data10 = []
 
     for i in range(N):
         y = coor[i,1]
         x = coor[i,0]
-
+        row.append(i); col.append(i); data10.append(0); data01.append(0)
         if y <= Wsc:
-            D[i,i] = delta*np.exp( -1j*phi/2 )
+            row.append(i); col.append(i); data10.append( -delta*np.exp( -1j*phi/2 ) )
+            data01.append(delta*np.exp( -1j*phi/2 ) )
 
         if y > Wsc and y <= (Wsc+Wj):
-            D[i,i] = 0
+            row.append(i); col.append(i); data10.append(0); data01.append(0)
 
         if y > (Wsc+Wj):
-            D[i,i] = delta*np.exp( 1j*phi/2 )
+            row.append(i); col.append(i); data10.append(-delta*np.exp( 1j*phi/2 ) )
+            data01.append(delta*np.exp( 1j*phi/2 ))
 
-    #Delta = delta*np.eye(N, N, dtype = 'complex')
-    D00 = np.zeros((N,N))
-    D11 = np.zeros((N,N))
-    D01 = D
-    D10 = -D
-    D = np.block([[D00, D01], [D10, D11]])
+    D01 = sparse.csc_matrix((data01, (row, col)), shape = (N,N), dtype = 'complex')
+    D10 = sparse.csr_matrix((data10, (row, col)), shape = (N,N), dtype = 'complex')
+    #D00 = sparse.csc_matrix((dataD, (row, col)), shape = (N,N), dtype = 'complex')
+    #D11 = sparse.csc_matrix((dataD, (row, col)), shape = (N,N), dtype = 'complex')
+
+    D = sparse.bmat([[None, D01], [D10, None]])
     return D
+
+    #D00 = np.zeros((N,N))
+    #D11 = np.zeros((N,N))
+    #D01 = D
+    #D10 = -D
+    #D = np.block([[D00, D01], [D10, D11]])
