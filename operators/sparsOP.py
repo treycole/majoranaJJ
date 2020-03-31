0fa4e73d3a33b676dc6e8ce83c2169f4233d7aad
Trey Cole <treco713@gmail.com>
2020-03-31 17:51:19 -0400

    some changes

--- operators/sparsOP.py
+++ operators/sparsOP.py
@@ -1,59 +1,98 @@
 import scipy.sparse as sparse
-
+import numpy as np
 import majoranaJJ.const as const
 
-def kx(coor, ax, ay, NN, NNb = None, periodic = 'no'):
+def kx(coor, ax, ay, NN, NNb = None, qx = 0):
     row = []; col = []; data = []
     N = coor.shape[0]
+
+    xmax = max(coor[:, 0])
+    xmin = max(coor[:, 0])
+
+    Lx = (xmax - xmin + 1)*ax
+    tx = 1j/2*ax
 
     for i in range(N):
         if NN[i,0] != -1:
-            row.append(NN[i,0]); col.append(i); data.append(-1j/2*ax)
+            row.append(NN[i,0]); col.append(i); data.append(-tx)
         if NN[i,2] != -1:
-            row.append(NN[i,2]); col.append(i); data.append(1j/2*ax)
-        if periodic.lower() == 'yes':
-            if NNb[i,0] != -1:
-                row.append(NN[i,0]); col.append(i); data.append(-1j/2*ax)
-            if NNb[i,2] != -1:
-                row.append(NN[i,2]); col.append(i); data.append(1j/2*ax)
+            row.append(NN[i,2]); col.append(i); data.append(tx)
+        if NNb != None and NNb[i,0] != -1:
+            row.append(NNb[i,0]); col.append(i); data.append(-tx*np.exp(-1j*qx*Lx))
+        if NNb != None and NNb[i,2] != -1:
+            row.append(NNb[i,2]); col.append(i); data.append(tx*np.exp(1j*qx*Lx))
 
     ksq = sparse.csr_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
     return ksq
 
-def kx2(coor, ax, ay, NN, NNb = None, periodic = 'no'):
+def kx2(coor, ax, ay, NN, NNb = None, qx = 0):
     row = []; col = []; data = []
     N = coor.shape[0]
+
+    xmax = max(coor[:, 0])
+    xmin = max(coor[:, 0])
+
+    Lx = (xmax - xmin + 1)*ax
+    tx = 1/ax**2
+
     for i in range(N):
-        row.append(i); col.append(i); data.append(2/ax**2)
+        row.append(i); col.append(i); data.append(2*tx)
         if NN[i,0] != -1:
-            row.append(NN[i,0]); col.append(i); data.append(-1./ax**2)
+            row.append(NN[i,0]); col.append(i); data.append(-tx)
         if NN[i,2] != -1:
-            row.append(NN[i,2]); col.append(i); data.append(-1./ax**2)
-        if
+            row.append(NN[i,2]); col.append(i); data.append(-tx)
+        if NNb != None and NNb[i,0] != -1:
+            row.append(NNb[i,0]); col.append(i); data.append(-tx*np.exp(-1j*qx*Lx))
+        if NNb != None and NNb[i,2] != -1:
+            row.append(NNb[i,2]); col.append(i); data.append(-tx*np.exp(1j*qx*Lx))
+
 
     ksq = sparse.csr_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
     return ksq
 
-def ky(coor, ax, ay, NN, NNb = None, periodic = 'no'):
+def ky(coor, ax, ay, NN, NNb = None, qy = 0):
     row = []; col = []; data = []
     N = coor.shape[0]
+
+    ymax = max(coor[:, 1])
+    ymin = max(coor[:, 1])
+
+    Ly = (ymax - ymin + 1)*ay
+    ty = 1j/2*ay
+
     for i in range(N):
         if NN[i,1] != -1:
-            row.append(NN[i,1]); col.append(i); data.append(-1j/2*ay)
+            row.append(NN[i,1]); col.append(i); data.append(-ty)
         if NN[i,3] != -1:
-            row.append(NN[i,3]); col.append(i); data.append(1j/2*ay)
+            row.append(NN[i,3]); col.append(i); data.append(ty)
+        if NNb != None and NNb[i,1] != -1:
+            row.append(NNb[i,1]); col.append(i); data.append(-ty*np.exp(-1j*qy*Ly))
+        if NNb != None and NNb[i,3] != -1:
+            row.append(NNb[i,3]); col.append(i); data.append(ty*np.exp(1j*qy*Ly))
+
     ksq = sparse.csr_matrix((data, (row,col)), shape = (N,N), dtype = 'complex')
     return ksq
 
-def ky2(coor, ax, ay, NN, NNb = None, periodic = 'no'):
+def ky2(coor, ax, ay, NN, NNb = None, qy = 0):
     row = []; col = []; data = []
     N = coor.shape[0]
+
+    ymax = max(coor[:, 1])
+    ymin = max(coor[:, 1])
+
+    Ly = (ymax - ymin + 1)*ay
+    ty = 1./ay**2
+
     for i in range(N):
-        row.append(i); col.append(i); data.append(2/ay**2)
+        row.append(i); col.append(i); data.append(2*ty)
         if NN[i,1] != -1:
-            row.append(NN[i,1]); col.append(i); data.append(-1./ay**2)
+            row.append(NN[i,1]); col.append(i); data.append(-ty)
         if NN[i,3] != -1:
-            row.append(NN[i,3]); col.append(i); data.append(-1./ay**2)
+            row.append(NN[i,3]); col.append(i); data.append(-ty)
+        if NNb != None and NNb[i,1] != -1:
+            row.append(NNb[i,1]); col.append(i); data.append(-ty*np.exp(-1j*qy*Ly))
+        if NNb != None and NNb[i,3] != -1:
+            row.append(NNb[i,3]); col.append(i); data.append(-ty*np.exp(1j*qy*Ly))
 
     ksq = sparse.csr_matrix((data, (row,col)), shape= (N,N), dtype = 'complex')
     return ksq
