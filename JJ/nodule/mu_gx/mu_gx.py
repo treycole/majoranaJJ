4960eca21089a1e639051c73e45c3e8dbc4d29b8
Trey Cole <treco713@gmail.com>
2020-05-21 20:25:00 -0400

    new gam finder

--- JJ/nodule/mu_gx/mu_gx.py
+++ JJ/nodule/mu_gx/mu_gx.py
@@ -10,21 +10,21 @@ import majoranaJJ.modules.plots as plots #plotting functions
 from majoranaJJ.modules.gamfinder import gamfinder as gf
 from majoranaJJ.operators.potentials.barrier_leads import V_BL
 
-Nx = 20 #Number of lattice sites along x-direction
-Ny = 350 #Number of lattice sites along y-direction
+Nx = 4 #Number of lattice sites along x-direction
+Ny = 150 #Number of lattice sites along y-direction
 ax = 50 #lattice spacing in x-direction: [A]
 ay = 50 #lattice spacing in y-direction: [A]
-Wj = 4 #Junction region
-Sx = 8 #3 #length of either side of nodule
+Wj = 10 #Junction region
+Sx = int(Nx/2) #3 #length of either side of nodule
 cutx = (Nx - 2*Sx) #width of nodule
-cuty = 1 #4 #height of nodule
+cuty = 0 #4 #height of nodule
 
 Junc_width = Wj*ay*.10 #nm
 SC_width = ((Ny - Wj)*ay*.10)/2 #nm
-#Nod_widthx = cutx*ax*.1 #nm
-#Nod_widthy = cuty*ay*.1 #nm
-#print("Nodule Width in x-direction = ", Nod_widthx, "(nm)")
-#print("Nodule Width in y-direction = ", Nod_widthy, "(nm)")
+Nod_widthx = cutx*ax*.1 #nm
+Nod_widthy = cuty*ay*.1 #nm
+print("Nodule Width in x-direction = ", Nod_widthx, "(nm)")
+print("Nodule Width in y-direction = ", Nod_widthy, "(nm)")
 print("Junction Width = ", Junc_width, "(nm)")
 print("Supercondicting Lead Width = ", SC_width, "(nm)")
 
@@ -36,17 +36,48 @@ print("Lattice Size: ", lat_size)
 
 Lx = (max(coor[:, 0]) - min(coor[:, 0]) + 1)*ax #Unit cell size in x-direction
 Ly = (max(coor[:, 1]) - min(coor[:, 1]) + 1)*ay #Unit cell size in y-direction
+###############################################################################
 
-steps = 50
+#Defining Hamiltonian parameters
+steps = 200
 
 alpha = 200 #Spin-Orbit Coupling constant: [meV*A]
-gammax_i = 0 #parallel to junction: [meV]
-gammaz = 0.0
+gammax_i = 0.0001 #parallel to junction: [meV]
 phi = np.pi #SC phase difference
 delta = 1.0 #Superconducting Gap: [meV]
-V0 = -5 #50 #Amplitude of potential : [meV]
+V0 = 0 #Amplitude of potential : [meV]
 V = V_BL(coor, Wj = Wj, Sx = Sx, cutx=cutx, cuty=cuty, V0 = V0)
-mu = np.linspace(0, 40, steps) #Chemical Potential: [meV], 20
+mu = np.linspace(0, 20, steps) #Chemical Potential: [meV], 20
+
+###############################################################################
+
+#phase diagram mu vs gamx
+gamx_0 = []
+gamx_pi = []
+mu_arr = []
+for i in range(steps):
+    print(steps-i)
+
+    gammaxpi = gf(
+        coor, ax, ay, NN, mu[i], NNb = NNb, Wj = Wj, Sx = Sx, cutx = cutx, cuty=cuty, alpha = alpha, delta = delta, phi = np.pi, gammax = gammax_i, V = V, periodicX = True
+        )
+
+    mu_arr.append(mu[i])
+    gamx_pi.append(gammaxpi)
+
+mu_arr, gamx_pi = np.array(mu_arr), np.array(gamx_pi)
+print(gamx_pi.shape, mu_arr.shape)
+
+plt.plot(gamx_pi, mu_arr, c='r', ls='solid')
+
+plt.xlabel(r'$E_z$ (meV)')
+plt.ylabel(r'$\mu$ (meV)')
+
+plt.legend()
+plt.savefig('oldgamfinder_juncwidth = {} SCwith = {} V0 = {} nodwidthx = {} nodwidthy = {} .png'.format(Junc_width, SC_width, V0, 0, 0 ))
+plt.show()
+
+###############################################################################
 
 """
 #state plot
@@ -69,31 +100,4 @@ plots.state_cmap(coor, eigs, states, n=4, savenm='prob_density_nodule_n=4.png')
 plots.state_cmap(coor, eigs, states, n=5, savenm='prob_density_nodule_n=5.png')
 plots.state_cmap(coor, eigs, states, n=6, savenm='prob_density_nodule_n=6.png')
 """
-
-"""
-#phase diagram mu vs gamx
-gamx_0 = []
-gamx_pi = []
-mu_arr = []
-for i in range(steps):
-    print(steps-i)
-
-    gammaxpi = gf(
-        coor, ax, ay, NN, mu[i], NNb = NNb, Wj = Wj, Sx = Sx, cutx = cutx, cuty=cuty, alpha = alpha, delta = delta, phi = np.pi, gammax = gammax_i, V = V, periodicX = True, k=12
-        )
-
-    mu_arr.append(mu[i])
-    gamx_pi.append(gammaxpi)
-
-mu_arr, gamx_pi = np.array(mu_arr), np.array(gamx_pi)
-print(gamx_pi.shape, mu_arr.shape)
-
-plt.plot(gamx_pi, mu_arr, c='r', ls='solid')
-
-plt.xlabel(r'$E_z$ (meV)')
-plt.ylabel(r'$\mu$ (meV)')
-
-plt.legend()
-plt.savefig('juncwidth = {} SCwith = {} V0 = {} nodwidthx = {} nodwidthy = {} .png'.format(Junc_width, SC_width, V0, 0, 0 ))
-plt.show()
-"""
+###############################################################################
--- modules/gamfinder.py
+++ modules/gamfinder.py
@@ -1,9 +1,11 @@
 import majoranaJJ.operators.sparse.qmsops as spop #sparse operators
 import numpy as np
+import scipy.linalg as LA
+import scipy.sparse.linalg as spLA
 
 def gamfinder(
     coor, ax, ay, NN, mu, NNb = None, Wj = 0, Sx = None, cutx = None, cuty = None, V = 0, gammax = 0, gammay = 0, gammaz = 0, alpha = 0, delta = 0 , phi = 0, qx = 0, qy = 0, periodicX = False, periodicY = False,
-    k = 2, sigma = 0, which = 'LM', tol = 0, maxiter = None
+    k = 20, sigma = 0, which = 'LM', tol = 0, maxiter = None
     ):
 
     #saving the particle energies, all energies above E=0
@@ -40,5 +42,57 @@ def gamfinder(
     b = np.array(Ei - m*gammax) #y-intercept
     G_crit = np.array(-b/m) #gamma value that E=0 for given mu value
     #print(G_crit)
+
+    return G_crit
+
+"""
+This function calculates the phase transition points. To work it needs energy eigenvalues and eigenvectors of unperturbed Hamiltonian, or a Hamiltonian without any Zeeman field. When a Zeeman field is turned on, perturbation theory can be applied to calculate the new energy eigenvalues and eigenvectors.
+"""
+
+def gamfinder_lowE(
+    coor, ax, ay, NN, mu, NNb = None, Wj = 0, Sx = None, cutx = None, cuty = None, V = 0, gammax = 0, gammay = 0, gammaz = 0, alpha = 0, delta = 0 , phi = 0, qx = 0, qy = 0, periodicX = False, periodicY = False,
+    k = 20, sigma = 0, which = 'LM', tol = 0, maxiter = None
+    ):
+
+    MU = mu #fixed mu value
+    k0 = k #64 #perturbation energy eigs
+    gx = np.linspace(0, 3, 5000) #just want two values that are close together in one step in the linspace
+
+    H0 = spop.HBDG(coor, ax, ay, NN, NNb=NNb, Wj=Wj, Sx=Sx, cutx=cutx, cuty=cuty, V=V, mu=MU, gammax=0.00001, alpha=alpha, delta=delta, phi=phi, qx=qx, qy=qy, periodicX=periodicX, periodicY=periodicY) #
+
+    eigs_0, vecs_0 = spLA.eigsh(H0, k=k0, sigma=0, which='LM')
+    vecs_0_hc = np.conjugate(np.transpose(vecs_0))
+
+    H_G0 =  spop.HBDG(coor, ax, ay, NN, NNb = NNb, Wj = Wj, Sx = Sx, cutx = cutx, cuty = cuty, V = V, mu = MU, gammax = 0, alpha = alpha, delta = delta, phi = phi, qx = qx, qy=qy, periodicX = periodicX, periodicY=periodicY)
+
+    H_G1 = spop.HBDG(coor, ax, ay, NN, NNb = NNb, Wj = Wj, Sx = Sx, cutx = cutx, cuty = cuty, V = V, mu = MU, gammax = 1, alpha = alpha, delta = delta, phi = phi, qx = qx, qy=qy, periodicX = periodicX, periodicY=periodicY)
+
+    #the proporitonality matrix for gammax, it is ones along the sites that have a gamma value
+
+    HG = H_G1 - H_G0
+
+    eig_arr = np.zeros((gx.shape[0], k))
+    eig_arr_NB = np.zeros((gx.shape[0], k0))
+
+
+    for i in range(2):
+
+        H = H_G0 + gx[i]*HG #saves us from having to run matrix constructors in each loop. Still exact
+
+        H_dif_basis = np.dot(vecs_0_hc, H.dot(vecs_0)) # H' = U^dagger H U
+        eigs_dif_basis, U_dif_basis = LA.eigh(H_dif_basis)
+
+        eig_arr_NB[i, :] = eigs_dif_basis
+
+
+    Ei = eig_arr_NB[0, :][int(k/2):][::2]
+    Ef = eig_arr_NB[1, :][int(k/2):][::2]
+
+    gammax = gx[0]
+    gammanew = gx[1]
+
+    m = np.array((Ef - Ei)/(gammanew - gammax)) #slope, linear dependence on gamma
+    b = np.array(Ei - m*gammax) #y-intercept
+    G_crit = np.array(-b/m) #gamma value that E=0 for given mu value
 
     return G_crit
